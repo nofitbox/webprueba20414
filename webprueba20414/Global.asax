6b3e419b6f96723a433de2a8e7eb5a526f18376f
nofitbox <nofitbox@gmail.com>
2014-04-20 19:47:30 -0400

    Initial commit

---
+++ webprueba20414/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="webprueba20414.MvcApplication" Language="C#" %>
---
+++ webprueba20414/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webprueba20414.Startup))]
+namespace webprueba20414
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
